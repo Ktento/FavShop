dfdaa2a86c5640dbcc2491e4d5a1c1726f359aae
Ktento <kento07210525mo@outlook.jp>
2024-07-19 21:05:30 +0900

    店舗検索の処理を追加

---
+++ FavShop/backend/find_Shop.ts
@@ -0,0 +1,36 @@
+import axios from "axios";
+require('dotenv').config();
+interface Place {
+    name: string;
+    address: string;
+    location: {
+      lat: number;
+      lng: number;
+    };
+}
+let BASE_URL="https://maps.googleapis.com/maps/api/place/textsearch/json";
+let API_KEY="AIzaSyBz5FX9M10pvm3UAiECGW_3HrysEWfFzws";
+
+
+export const searchPlaceByName = async (Shopname:string):Promise<Place[]> => {
+    const response=await axios.get(BASE_URL,{
+        params:{
+            key:API_KEY,
+            query:Shopname,
+        },
+    });
+    return response.data.results.map((place: any) => ({
+        name: place.name,
+        address: place.formatted_address,
+        location: place.geometry.location,
+      }));
+}
+
+
+/*
+const placeName = 'セブンイレブン'; // 検索したい店の名前
+searchPlaceByName(placeName).then(places => {
+  console.log('Found places:', places);
+});
+
+*/
