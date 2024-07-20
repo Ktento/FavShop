172ac8d83bdc7b3c5d28e954f0375e8f2df64c46
Ktento <kento07210525mo@outlook.jp>
2024-07-20 14:19:33 +0900

    vercelのサーバーレス関数を作成

---
+++ FavShop/frontend/api/search-places.ts
@@ -0,0 +1,30 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+import fetch from 'node-fetch';
+
+const BASE_URL: string = process.env.REACT_APP_GOOGLE_PLACES_API_URL ?? "";
+const API_KEY: string = process.env.REACT_APP_GOOGLE_PLACES_API_KEY ?? "";
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  const query = req.query.query as string;
+
+  if (!query) {
+    return res.status(400).json({ error: 'Query parameter is required' });
+  }
+
+  if (!API_KEY) {
+    return res.status(500).json({ error: 'Google Places API key is not defined' });
+  }
+
+  try {
+    const response = await fetch(`${BASE_URL}?key=${API_KEY}&query=${encodeURIComponent(query)}`);
+    const data = await response.json();
+
+    if (!data.results) {
+      return res.status(500).json({ error: 'Invalid API response format' });
+    }
+
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching data from Google Places API' });
+  }
+}
