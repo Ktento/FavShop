ef535cd023f45de3a77e5ce4ff7a81af614511f8
Ktento <kento07210525mo@outlook.jp>
2024-07-30 18:37:37 +0900

    test

--- FavShop/frontend/src/components/Info_Modal.tsx
+++ FavShop/frontend/src/components/Info_Modal.tsx
@@ -10,23 +10,18 @@ interface InfoModalProps {
 const InfoModal: React.FC<InfoModalProps> = ({ open, onClose }) => {
   return (
     <Modal isOpen={open} onClose={onClose}>
-      <div className="info-modal-content">
+      <div className="modal-content">
         <div className="modal-header">
           <Typography variant="h6" className="modal-title">
             アプリについて
           </Typography>
         </div>
-        <div className="modal-content">
-          <Typography variant="body2">
+        <div className="info-modal-content">
+          <Typography variant="body1">
             このアプリケーションは、ユーザーが様々な機能を利用できるように設計されています。
+            ここでは、アプリの簡単な説明や、主要な機能について説明します。
+            例えば、ユーザー登録やログイン、店舗の検索機能などが含まれます。
           </Typography>
-          <ul className="info-modal-list">
-            <li><strong>近くの店舗表示機能:</strong> ログアウト状態では周辺の店舗が初期状態として表示されます。</li>
-            <li><strong>ログイン機能:</strong> アカウント登録がされているユーザはログインすることができます。サイドバーメニューからログインをしてください。</li>
-            <li><strong>サインイン機能:</strong> アカウントの登録をすることができます。ユーザ名、パスワードは半角英数字で未登録の名前を設定する必要があります。</li>
-            <li><strong>お気に入りの店舗登録機能:</strong> サイドバーメニューの+を押すことで店舗の検索メニューが表示されます。検索キーワードは店舗名 地名を入力することでより正確な店舗検索ができます。検索ボタンを押すと検索が開始されます。表示された店舗を選択し決定ボタンを押すことで店舗の追加ができます。※店舗の追加はログイン中でかつ未登録の店舗でないと実行できません</li>
-            <li><strong>お気に入りの店舗の表示:</strong> 登録されたお気に入りの店舗が現在地から<strong>近い順</strong>に表示されます。赤色→営業終了、黄色→営業終了30分前、青色→営業中を示しています。店舗をクリックすることで詳細を確認できます。店舗までの経路→現在地からの経路をgooglemapで表示します。店舗詳細を確認→店舗のWebサイトが表示されます。</li>
-          </ul>
         </div>
       </div>
     </Modal>
