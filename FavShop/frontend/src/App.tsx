e5223c90fd7b7e1887137a8904d4d133c2564c2f
Ktento <kento07210525mo@outlook.jp>
2024-08-13 08:23:04 +0900

    fetchNearbyShops をuseEffectの処理外に移動し関数として定義

--- FavShop/frontend/src/App.tsx
+++ FavShop/frontend/src/App.tsx
@@ -1,48 +1,59 @@
 // App.tsx
-import React, { useState ,useEffect} from 'react';
-import './App.css';
-import Header from './components/Header';
-import Content from './components/Content';
-import { SearchNearShops } from './backend/find_near_Shops';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Content from "./components/Content";
+import { SearchNearShops } from "./backend/find_near_Shops";
 export interface CardData {
-  id: number|null;
+  id: number | null;
   plaseid: string;
   image: string;
   title: string;
   address: string;
-  webURL:string;
+  webURL: string;
   hours: string;
 }
 const App: React.FC = () => {
-  console.log("Content OPEN")
-  const [user, setUser] = useState<string | null>(localStorage.getItem('user') || null);
-  const [user_id, setUserID] = useState<number | null>(localStorage.getItem('user_id') ? parseInt(localStorage.getItem('user_id')!) : null);
+  console.log("Content OPEN");
+  const [user, setUser] = useState<string | null>(
+    localStorage.getItem("user") || null
+  );
+  const [user_id, setUserID] = useState<number | null>(
+    localStorage.getItem("user_id")
+      ? parseInt(localStorage.getItem("user_id")!)
+      : null
+  );
   /*カードデータを保持するための配列を定義*/
   const [carddata, setCardData] = useState<CardData[]>(() => {
     // 初期状態としてlocalStorageからカードデータを取得
-    const savedCardData = localStorage.getItem('carddata');
+    const savedCardData = localStorage.getItem("carddata");
     return savedCardData ? JSON.parse(savedCardData) : [];
   });
 
   /*現在地を保持*/
-  const [location, setLocation] = useState<{ latitude: number|null; longitude: number|null } | null>(null);
+  const [location, setLocation] = useState<{
+    latitude: number | null;
+    longitude: number | null;
+  } | null>(null);
 
   // カード情報を追加する関数
   const addCardData = (newCard: CardData) => {
     //setCardData(prevCardData => [...prevCardData, newCard]);
-    setCardData(prevCardData => {
+    setCardData((prevCardData) => {
       const updatedCardData = [...prevCardData, newCard];
-      localStorage.setItem('carddata', JSON.stringify(updatedCardData)); // localStorageに保存
+      localStorage.setItem("carddata", JSON.stringify(updatedCardData)); // localStorageに保存
       return updatedCardData;
     });
   };
-  
+
   // カード情報を削除する関数
   const deleteCardData = (place_id: string) => {
     //setCardData(prevCardData => prevCardData.filter(card => card.plaseid !== place_id));
-    setCardData(prevCardData => {
-      const updatedCardData = prevCardData.filter(card => card.plaseid !== place_id);
-      localStorage.setItem('carddata', JSON.stringify(updatedCardData)); // localStorageに保存
+    setCardData((prevCardData) => {
+      const updatedCardData = prevCardData.filter(
+        (card) => card.plaseid !== place_id
+      );
+      localStorage.setItem("carddata", JSON.stringify(updatedCardData)); // localStorageに保存
       return updatedCardData;
     });
   };
@@ -66,58 +77,74 @@ const App: React.FC = () => {
     // 位置情報を取得
     getCurrentPosition();
   }, []); // 空の依存配列でマウント時に一度だけ実行
+  //現在地から近い店舗のカード情報を登録する関数
+  const fetchNearbyShops = async () => {
+    if (location?.latitude && location?.longitude && carddata.length === 0) {
+      try {
+        const card = await SearchNearShops(
+          location.latitude,
+          location.longitude
+        );
+        setCardData(card);
+      } catch (error) {
+        console.error("Error fetching nearby shops:", error);
+      }
+    } else {
+      setCardData([]);
+    }
+  };
 
   useEffect(() => {
-    const fetchNearbyShops = async () => {
-      if (location?.latitude && location?.longitude && carddata.length === 0) {
-        try {
-          const card = await SearchNearShops(location.latitude, location.longitude);
-          setCardData(card);
-        } catch (error) {
-          console.error('Error fetching nearby shops:', error);
-        }
-      }
-    };
-
     fetchNearbyShops();
   }, [location, carddata]);
 
   // ログイン状態を localStorage に保存
   useEffect(() => {
     if (user !== null) {
-      localStorage.setItem('user', user);
+      localStorage.setItem("user", user);
     } else {
-      localStorage.removeItem('user');
+      localStorage.removeItem("user");
     }
   }, [user]);
 
   useEffect(() => {
     if (user_id !== null) {
-      localStorage.setItem('user_id', user_id.toString());
+      localStorage.setItem("user_id", user_id.toString());
     } else {
-      localStorage.removeItem('user_id');
+      localStorage.removeItem("user_id");
     }
   }, [user_id]);
 
   useEffect(() => {
-    console.log("useEffect card set")
-    localStorage.setItem('carddata', JSON.stringify(carddata)); 
+    console.log("useEffect card set");
+    localStorage.setItem("carddata", JSON.stringify(carddata));
   }, [carddata]);
 
-
-  
-  
   return (
     <div className="App">
-      <Header user={user} user_id={user_id} location={location} 
-      setUser={setUser} setUserID={setUserID} 
-      carddata={carddata} setCardData={setCardData} addCardData={addCardData}/>
+      <Header
+        user={user}
+        user_id={user_id}
+        location={location}
+        setUser={setUser}
+        setUserID={setUserID}
+        carddata={carddata}
+        setCardData={setCardData}
+        addCardData={addCardData}
+        fetchNearbyShops={fetchNearbyShops}
+      />
       <div className="main">
-        <Content user_id={user_id} location={location} 
-        carddata={carddata} setCardData={setCardData} addCardData={addCardData} deleteCardData={deleteCardData}/>
+        <Content
+          user_id={user_id}
+          location={location}
+          carddata={carddata}
+          setCardData={setCardData}
+          addCardData={addCardData}
+          deleteCardData={deleteCardData}
+        />
       </div>
     </div>
   );
-}
+};
 
 export default App;
