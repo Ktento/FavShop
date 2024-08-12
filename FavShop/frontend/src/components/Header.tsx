53cec20c1d02d96f101913c35e9426275552359c
Ktento <kento07210525mo@outlook.jp>
2024-08-13 08:24:08 +0900

    ログアウト機能の実装

--- FavShop/frontend/src/components/Header.tsx
+++ FavShop/frontend/src/components/Header.tsx
@@ -1,34 +1,47 @@
-import React, { useState } from 'react';
-import { Drawer, IconButton, AppBar, Toolbar, Typography } from '@mui/material';
-import MenuIcon from '@mui/icons-material/Menu';
-import Sidebar from './Sidebar';
-import logo from '../assets/images/Favshop_logo_white.png';
-import '../CSS/Header.css';
-import { CardData } from '../App';
+import React, { useState } from "react";
+import { Drawer, IconButton, AppBar, Toolbar, Typography } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import Sidebar from "./Sidebar";
+import logo from "../assets/images/Favshop_logo_white.png";
+import "../CSS/Header.css";
+import { CardData } from "../App";
 interface HeaderProps {
-  user: string | null; 
-  user_id: number | null; 
-  location :{latitude:number|null, longitude:number|null}|null;
+  user: string | null;
+  user_id: number | null;
+  location: { latitude: number | null; longitude: number | null } | null;
   setUser: (user: string | null) => void;
   setUserID: (user_id: number | null) => void;
-  carddata : CardData[]|null;
+  carddata: CardData[] | null;
   //CardData配列をすべて初期化するか、配列の一つを更新するか選べる
-  setCardData:React.Dispatch<React.SetStateAction<CardData[]>>;
+  setCardData: React.Dispatch<React.SetStateAction<CardData[]>>;
   addCardData: (newCard: CardData) => void;
+  fetchNearbyShops: () => void;
 }
 
-const Header: React.FC<HeaderProps> = ({ user, user_id,carddata,location,setUser,setUserID,setCardData,addCardData}) => {
+const Header: React.FC<HeaderProps> = ({
+  user,
+  user_id,
+  carddata,
+  location,
+  setUser,
+  setUserID,
+  setCardData,
+  addCardData,
+  fetchNearbyShops,
+}) => {
   const [drawerOpen, setDrawerOpen] = useState(false);
 
-  const toggleDrawer = (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
-    if (
-      event.type === 'keydown' &&
-      ((event as React.KeyboardEvent).key === 'Tab' || (event as React.KeyboardEvent).key === 'Shift')
-    ) {
-      return;
-    }
-    setDrawerOpen(open);
-  };
+  const toggleDrawer =
+    (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (
+        event.type === "keydown" &&
+        ((event as React.KeyboardEvent).key === "Tab" ||
+          (event as React.KeyboardEvent).key === "Shift")
+      ) {
+        return;
+      }
+      setDrawerOpen(open);
+    };
 
   const closeDrawer = () => {
     setDrawerOpen(false);
@@ -38,16 +51,22 @@ const Header: React.FC<HeaderProps> = ({ user, user_id,carddata,location,setUser
     <>
       <AppBar position="fixed" className="header">
         <Toolbar className="toolbar">
-          <IconButton edge="start" color="inherit" aria-label="menu" onClick={toggleDrawer(true)} className="menu-button">
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            onClick={toggleDrawer(true)}
+            className="menu-button"
+          >
             <MenuIcon />
           </IconButton>
           <img src={logo} alt="Logo" className="header-logo" />
           <Typography variant="h6" component="div" className="title">
-          {user
+            {user
               ? `ログイン中:${user}`
               : location
-              ? '近くの店舗を表示中'
-              : '未ログイン'}
+                ? "近くの店舗を表示中"
+                : "未ログイン"}
           </Typography>
         </Toolbar>
       </AppBar>
@@ -56,14 +75,21 @@ const Header: React.FC<HeaderProps> = ({ user, user_id,carddata,location,setUser
         open={drawerOpen}
         onClose={toggleDrawer(false)}
         PaperProps={{
-          sx: { width: { xs: '100%', sm: 250 } }, // スマホでは100%、デスクトップでは250pxの幅
+          sx: { width: { xs: "100%", sm: 250 } }, // スマホでは100%、デスクトップでは250pxの幅
         }}
       >
- 
-        <Sidebar user={user} user_id={user_id} setUser={setUser} setUserID={setUserID} 
-        carddata={carddata} setCardData={setCardData} addCardData={addCardData}
-        closeDrawer={closeDrawer} 
-        location={location}/>
+        <Sidebar
+          user={user}
+          user_id={user_id}
+          setUser={setUser}
+          setUserID={setUserID}
+          carddata={carddata}
+          setCardData={setCardData}
+          addCardData={addCardData}
+          closeDrawer={closeDrawer}
+          location={location}
+          fetchNearbyShops={fetchNearbyShops}
+        />
       </Drawer>
     </>
   );
--- FavShop/frontend/src/components/Sidebar.tsx
+++ FavShop/frontend/src/components/Sidebar.tsx
@@ -1,32 +1,45 @@
-import React, { useState } from 'react';
-import { IconButton } from '@mui/material';
-import CloseIcon from '@mui/icons-material/Close';
-import Footer from './Footer';
-import Modal from './Modal';
-import Modal2 from './Modal2';
-import SignInModal from './SignIn_Modal';
-import SignUpModal from './SignUp_Modal';
-import PositionModal from './Position_Modal';
-import AddModal from './Add_Modal';
-import InfoModal from './Info_Modal';
-import '../CSS/Sidebar.css';
-import button_position_Image from '../assets/images/Map pin.png';
-import button_add_Image from '../assets/images/Plus.png';
-import { CardData } from '../App';
+import React, { useState } from "react";
+import { IconButton } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+import Footer from "./Footer";
+import Modal from "./Modal";
+import Modal2 from "./Modal2";
+import SignInModal from "./SignIn_Modal";
+import SignUpModal from "./SignUp_Modal";
+import PositionModal from "./Position_Modal";
+import AddModal from "./Add_Modal";
+import InfoModal from "./Info_Modal";
+import "../CSS/Sidebar.css";
+import button_position_Image from "../assets/images/Map pin.png";
+import button_add_Image from "../assets/images/Plus.png";
+import { CardData } from "../App";
+import { SearchNearShops } from "../backend/find_near_Shops";
 interface SidebarProps {
-  user: string | null; 
+  user: string | null;
   user_id: number | null;
-  location :{latitude:number|null, longitude:number|null}|null;
+  location: { latitude: number | null; longitude: number | null } | null;
   setUser: (user: string | null) => void;
   setUserID: (user_id: number | null) => void;
   closeDrawer: () => void;
-  carddata : CardData[]|null;
+  carddata: CardData[] | null;
   //CardData配列をすべて初期化するか、配列の一つを更新するか選べる
-  setCardData:React.Dispatch<React.SetStateAction<CardData[]>>;
+  setCardData: React.Dispatch<React.SetStateAction<CardData[]>>;
   addCardData: (newCard: CardData) => void;
+  fetchNearbyShops: () => void;
 }
 
-const Sidebar: React.FC<SidebarProps> = ({ user,user_id,carddata,location,setUser,setUserID, setCardData,addCardData,closeDrawer }) => {
+const Sidebar: React.FC<SidebarProps> = ({
+  user,
+  user_id,
+  carddata,
+  location,
+  setUser,
+  setUserID,
+  setCardData,
+  addCardData,
+  closeDrawer,
+  fetchNearbyShops,
+}) => {
   const [signInModalOpen, setSignInModalOpen] = useState(false);
   const [signUpModalOpen, setSignUpModalOpen] = useState(false);
   const [addModalOpen, setAddModalOpen] = useState(false);
@@ -73,13 +86,25 @@ const Sidebar: React.FC<SidebarProps> = ({ user,user_id,carddata,location,setUse
     setInfoModalOpen(false);
   };
 
+  const Logout = (): void => {
+    try {
+      setUser(null);
+      setUserID(null);
+      //carddataがnullでない場合のみ実行
+      if (carddata) fetchNearbyShops();
+      alert("ログアウト完了");
+    } catch {
+      alert("ログアウトできませんでした");
+    }
+  };
+
   return (
     <div className="sidebar">
       <IconButton
         aria-label="close"
         onClick={closeDrawer}
         sx={{
-          position: 'absolute',
+          position: "absolute",
           right: 8,
           top: 8,
           color: (theme) => theme.palette.grey[500],
@@ -88,31 +113,64 @@ const Sidebar: React.FC<SidebarProps> = ({ user,user_id,carddata,location,setUse
         <CloseIcon />
       </IconButton>
       <div className="sidebar-content">
-        <button className="sidebar-button text-button" onClick={openInfoModal}>FavShopとは</button>
-        <button className="sidebar-button text-button" onClick={openSignInModal}>Sign In</button>
+        <button className="sidebar-button text-button" onClick={openInfoModal}>
+          FavShopとは
+        </button>
+        <button
+          className="sidebar-button text-button"
+          onClick={openSignInModal}
+        >
+          Sign In
+        </button>
         <button className="sidebar-button" onClick={openAddModal}>
           <img src={button_add_Image} alt="add" className="button-image" />
         </button>
         <button className="sidebar-button" onClick={openPositionModal}>
-          <img src={button_position_Image} alt="position" className="button-image" />
+          <img
+            src={button_position_Image}
+            alt="position"
+            className="button-image"
+          />
+        </button>
+        <button
+          className="sidebar-button text-button"
+          onClick={Logout}
+          style={{ color: "red" }}
+        >
+          Log out
         </button>
       </div>
       <Footer />
 
       <Modal isOpen={signInModalOpen} onClose={closeSignInModal}>
-        <SignInModal openSignUpModal={openSignUpModal} onClose={closeSignInModal} 
-        user={user} user_id={user_id} setUser={setUser} setUserID={setUserID}
-        location={location} 
-        carddata={carddata} setCardData={setCardData}
-        closeDrawer={closeDrawer}/>
+        <SignInModal
+          openSignUpModal={openSignUpModal}
+          onClose={closeSignInModal}
+          user={user}
+          user_id={user_id}
+          setUser={setUser}
+          setUserID={setUserID}
+          location={location}
+          carddata={carddata}
+          setCardData={setCardData}
+          closeDrawer={closeDrawer}
+        />
       </Modal>
       <Modal2 isOpen={signUpModalOpen} onClose={closeSignUpModal}>
-        <SignUpModal onClose={closeSignUpModal}/>
+        <SignUpModal onClose={closeSignUpModal} />
       </Modal2>
       <Modal isOpen={addModalOpen} onClose={closeAddModal}>
-        <AddModal onClose={closeAddModal} closeDrawer={closeDrawer} 
-        user={user} user_id={user_id} setUser={setUser} setUserID={setUserID}
-        carddata={carddata} setCardData={setCardData} addCardData={addCardData}/>
+        <AddModal
+          onClose={closeAddModal}
+          closeDrawer={closeDrawer}
+          user={user}
+          user_id={user_id}
+          setUser={setUser}
+          setUserID={setUserID}
+          carddata={carddata}
+          setCardData={setCardData}
+          addCardData={addCardData}
+        />
       </Modal>
       <Modal isOpen={positionModalOpen} onClose={closePositionModal}>
         <PositionModal onClose={closePositionModal} closeDrawer={closeDrawer} />
